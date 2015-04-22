3864cc31a0a6f171544616c514a8df81d87f334a
ANDRES KWAN <andreskwan@gmail.com>
2015-04-21 22:49:04 -0500

    React - Managing & Storing Your Data

--- Ray-BestPractices-SimpleWeather/Model/WXManager.m
+++ Ray-BestPractices-SimpleWeather/Model/WXManager.m
@@ -7,7 +7,67 @@
 //
 
 #import "WXManager.h"
+#import "WXClient.h"
+#import <TSMessages/TSMessage.h>
+
+@interface WXManager ()
+
+// 1
+@property (nonatomic, strong, readwrite) WXCondition *currentCondition;
+@property (nonatomic, strong, readwrite) CLLocation *currentLocation;
+@property (nonatomic, strong, readwrite) NSArray *hourlyForecast;
+@property (nonatomic, strong, readwrite) NSArray *dailyForecast;
+
+// 2
+@property (nonatomic, strong) CLLocationManager *locationManager;
+@property (nonatomic, assign) BOOL isFirstUpdate;
+@property (nonatomic, strong) WXClient *client;
+
+@end
 
 @implementation WXManager
+
+- (id)init {
+    if (self = [super init]) {
+        // 1
+        _locationManager = [[CLLocationManager alloc] init];
+        _locationManager.delegate = self;
+        
+        // 2
+        _client = [[WXClient alloc] init];
+        
+        // 3
+        [[[[RACObserve(self, currentLocation)
+            // 4
+            ignore:nil]
+           // 5
+           // Flatten and subscribe to all 3 signals when currentLocation updates
+           flattenMap:^(CLLocation *newLocation) {
+               return [RACSignal merge:@[
+                                         [self updateCurrentConditions],
+                                         [self updateDailyForecast],
+                                         [self updateHourlyForecast]
+                                         ]];
+               // 6
+           }] deliverOn:RACScheduler.mainThreadScheduler]
+         // 7
+         subscribeError:^(NSError *error) {
+             [TSMessage showNotificationWithTitle:@"Error"
+                                         subtitle:@"There was a problem fetching the latest weather."
+                                             type:TSMessageNotificationTypeError];
+         }];
+    }
+    return self;
+}
+
++ (instancetype)sharedManager {
+    static id _sharedManager = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        _sharedManager = [[self alloc] init];
+    });
+    
+    return _sharedManager;
+}
 
 @end
